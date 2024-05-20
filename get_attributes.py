a2a6e17e57040d4395e114edb6f08c3a3e970ea8
Ryan Sandridge <noreply@ryansandridge.com>
2024-05-20 15:38:20 -0400

    Oauth2 test

---
+++ get_attributes.py
@@ -0,0 +1,21 @@
+import requests
+from pprint import pprint
+
+# Define the file path
+file_path = ".api_token"
+
+# Read the content of the file into a variable
+with open(file_path, "r") as file:
+    TOKEN = file.read().strip()
+
+url = 'https://exist.io/api/2/attributes/with-values/'
+
+# make sure to authenticate ourselves with our token
+response = requests.get(url, headers={'Authorization': f'Token {TOKEN}'})
+
+if response.status_code == 200:
+    data = response.json()
+    # pretty print the json
+    pprint(data)
+else:
+    print("Error!", response.content)
---
+++ request_authorization.py
@@ -0,0 +1,23 @@
+from urllib.parse import urlencode
+
+URL = 'https://exist.io/oauth2/authorize'
+REDIRECT_URI = 'http://localhost:8000/'
+
+# Define the file path
+file_path = ".client_id"
+
+# Read the content of the file into a variable
+with open(file_path, "r") as file:
+    CLIENT_ID = file.read().strip()
+
+# the parameters we'll be sending
+params = {'client_id': CLIENT_ID,
+          'response_type':'code',
+          'redirect_uri':REDIRECT_URI,
+          'scope':'media_write',
+         }
+
+# let's encode them appropriately for a URL
+querystring = urlencode(params)
+# and finally print the complete result to the terminal
+print(f"{URL}?{querystring}")
